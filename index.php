d8018ec68385a6f2f38ea46a2d0bbda8f792146d
AlexeyShoshu <aliakseishoshu@gmail.com>
2023-06-09 17:42:30 +0300

    новая версия

--- index.php
+++ index.php
@@ -1,73 +1,20 @@
 <?php
+$dateCreate = '09.06.2023 16:12:12';
 
-$dateCreate = '30-06-2023 16:12:12';
-$pattern = "/^\d{2}[.-]\d{2}[.-]\d{4}\s\d{2}:\d{2}:\d{2}$/";
-
-list($date, $time) = explode(" ", $dateCreate);
-
-if (!stripos('.', $dateCreate)) {
-    list($day, $month, $year) = explode("-", $date);
-} else {
-    list($day, $month, $year) = explode(".", $date);
-}
-
-list($hour, $minute, $second) = explode(":", $time);
-
-$lastDayInMonth = date("t", strtotime($date));
-
-function isLastDayInMonth(&$day, $lastDayInMonth, &$dateNew, &$month, $year)
-{
-    if (($day == $lastDayInMonth) && ($month != 12)) {
-        $day = 0;
-        $month++;
-        $dateNew = implode(".", [$day, $month, $year]);
-    }
-}
-
-function isLastDayInYear(&$day, $lastDayInMonth, &$dateNew, &$month, &$year)
-{
-    if (($day == $lastDayInMonth) && ($month == 12)) {
-        $day = 0;
-        $month = 1;
-        $year++;
-        $dateNew = implode(".", [$day, $month, $year]);
-    }
-}
-
-function changeDate(&$day, $lastDayInMonth, &$dateNew, &$month, &$year) {
-    isLastDayInMonth($day, $lastDayInMonth, $dateNew, $month, $year);
-    isLastDayInYear($day, $lastDayInMonth, $dateNew, $month, $year);
-    $day++;
-    $dateNew = implode(".", [$day, $month, $year]);
-    while ((date("l", strtotime($dateNew)) == 'Saturday') || (date("l", strtotime($dateNew)) == 'Sunday')) {
-        isLastDayInMonth($day, $lastDayInMonth, $dateNew, $month, $year);
-        isLastDayInYear($day, $lastDayInMonth, $dateNew, $month, $year);
-        $day++;
-        $dateNew = implode(".", [$day, $month, $year]);
-    }
-}
-
-
-if ((!checkdate($month, $day, $year)) || (!preg_match($pattern, $dateCreate)) || (($hour < 0) || ($hour > 24)) || (($minute < 0) || ($minute > 59)) || (($second < 0) || ($second > 59))) {
+if (empty(strtotime($dateCreate))) {
     die("Дата не соответствует формату!");
 }
 
-if ($hour < 15) {
-    changeDate($day, $lastDayInMonth, $dateNew, $month, $year);
+if ((int) date('G', strtotime($dateCreate)) < 15) {
+    do {
+        $dateCreate = date("d.m.Y G:i:s", strtotime("+1 day", strtotime($dateCreate)));
+    } while ((date("l", strtotime($dateCreate)) == 'Saturday') || (date("l", strtotime($dateCreate)) == 'Sunday'));
 } else {
-    changeDate($day, $lastDayInMonth, $dateNew, $month, $year);
-    changeDate($day, $lastDayInMonth, $dateNew, $month, $year);
+    do {
+        $dateCreate = date("d.m.Y G:i:s", strtotime("+1 day", strtotime($dateCreate)));
+    } while ((date("l", strtotime($dateCreate)) == 'Saturday') || (date("l", strtotime($dateCreate)) == 'Sunday'));
+    $dateCreate = date("d.m.Y G:i:s", strtotime("+1 day", strtotime($dateCreate)));
 }
 
-if ($day <= 9) {
-    $day = '0' . $day;
-    $dateNew = implode(".", [$day, $month, $year]);
-}
-if (gettype($month) === 'integer') {
-    $month = '0' . $month;
-    $dateNew = implode(".", [$day, $month, $year]);
-}
+echo $dateCreate;
 
-$dateResult = implode(" ", [$dateNew, $time]);
-
-echo ($dateResult);
---
+++ oldChangeDate/date.php
@@ -0,0 +1,73 @@
+<?php
+
+$dateCreate = '30-03-2023 16:12:12';
+$pattern = "/^\d{2}[.-]\d{2}[.-]\d{4}\s\d{2}:\d{2}:\d{2}$/";
+
+list($date, $time) = explode(" ", $dateCreate);
+
+if (!stripos('.', $dateCreate)) {
+    list($day, $month, $year) = explode("-", $date);
+} else {
+    list($day, $month, $year) = explode(".", $date);
+}
+
+list($hour, $minute, $second) = explode(":", $time);
+
+$lastDayInMonth = date("t", strtotime($date));
+
+function isLastDayInMonth(&$day, $lastDayInMonth, &$dateNew, &$month, $year)
+{
+    if (($day == $lastDayInMonth) && ($month != 12)) {
+        $day = 0;
+        $month++;
+        $dateNew = implode(".", [$day, $month, $year]);
+    }
+}
+
+function isLastDayInYear(&$day, $lastDayInMonth, &$dateNew, &$month, &$year)
+{
+    if (($day == $lastDayInMonth) && ($month == 12)) {
+        $day = 0;
+        $month = 1;
+        $year++;
+        $dateNew = implode(".", [$day, $month, $year]);
+    }
+}
+
+function changeDate(&$day, $lastDayInMonth, &$dateNew, &$month, &$year) {
+    isLastDayInMonth($day, $lastDayInMonth, $dateNew, $month, $year);
+    isLastDayInYear($day, $lastDayInMonth, $dateNew, $month, $year);
+    $day++;
+    $dateNew = implode(".", [$day, $month, $year]);
+    while ((date("l", strtotime($dateNew)) == 'Saturday') || (date("l", strtotime($dateNew)) == 'Sunday')) {
+        isLastDayInMonth($day, $lastDayInMonth, $dateNew, $month, $year);
+        isLastDayInYear($day, $lastDayInMonth, $dateNew, $month, $year);
+        $day++;
+        $dateNew = implode(".", [$day, $month, $year]);
+    }
+}
+
+
+if ((!checkdate($month, $day, $year)) || (!preg_match($pattern, $dateCreate)) || (($hour < 0) || ($hour > 24)) || (($minute < 0) || ($minute > 59)) || (($second < 0) || ($second > 59))) {
+    die("Дата '{$dateCreate}' не соответствует формату!");
+}
+
+if ($hour < 15) {
+    changeDate($day, $lastDayInMonth, $dateNew, $month, $year);
+} else {
+    changeDate($day, $lastDayInMonth, $dateNew, $month, $year);
+    changeDate($day, $lastDayInMonth, $dateNew, $month, $year);
+}
+
+if ($day <= 9) {
+    $day = '0' . $day;
+    $dateNew = implode(".", [$day, $month, $year]);
+}
+if (gettype($month) === 'integer') {
+    $month = '0' . $month;
+    $dateNew = implode(".", [$day, $month, $year]);
+}
+
+$dateResult = implode(" ", [$dateNew, $time]);
+
+echo ($dateResult);
